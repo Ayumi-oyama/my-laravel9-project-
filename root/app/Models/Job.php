f8de98df0a6beab077442cf442213b731e2ee2f0
ayumi-oyama <ayumi.oyama@epkotsoftware.co.jp>
2024-12-24 12:07:48 +0900

    コメント削除

--- root/app/Models/Job.php
+++ root/app/Models/Job.php
@@ -2,8 +2,7 @@
 
 namespace App\Models;
 
-//Illuminate\Database\Eloquent\はフレームワーク。モデル間の関係を定義
-//データベース間のリレーションシップを定義するためのメソッドを提供
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
